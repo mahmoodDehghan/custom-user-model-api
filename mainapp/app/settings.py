edfc1c035b4810da32efcc484410d16624385b02
Mahmood D Monkabadi <m.mahmooddm@gmail.com>
2020-12-22 00:15:54 +0330

    set the custom user model based on email and password instead of username and password and implement the rest api token based for user

--- mainapp/app/settings.py
+++ mainapp/app/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core',
+    'user',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +126,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+AUTH_USER_MODEL = 'user.User'
